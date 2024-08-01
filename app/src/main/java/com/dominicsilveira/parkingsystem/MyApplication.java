7565272411089cf9cdf53c44743d6ae16805c719
Amol <amolchitrak2810@gmail.com>
2024-08-01 13:53:51 +0530

    uploade project

---
+++ app/src/main/java/com/dominicsilveira/parkingsystem/MyApplication.java
@@ -0,0 +1,13 @@
+package com.dominicsilveira.parkingsystem;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import com.dominicsilveira.parkingsystem.RegisterLogin.SplashScreen;
+import com.dominicsilveira.parkingsystem.utils.gps.GpsUtils;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MyApplication extends Application {
+}
